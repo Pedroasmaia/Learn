c8ab616ae613515eef6f64ae92a28c45128926bd
Pedro Maia <pedroasmaia@outlook.com>
2022-04-11 13:15:21 -0300

    Built In

---
+++ Python/ByLearn/2 - Listas/2. Métodos Alternativos de Criação de Listas.py
@@ -0,0 +1,12 @@
+#Meotods alternativos de criar listas:
+
+lista = [1,2,3,4]
+lista2 = ['a','b','c','d']
+lista_str_int = ['a','2','c','4']
+lista_vazia = []
+
+#Funnções Built in são funções externas:
+
+lista_built_in = list() #equivale a lista vazia
+variavel_para_lista = 12 #equivale a lista vazia
+lista_built_in_cheia = list(['a',2,variavel_para_lista]) #equivale a lista vazia
---
+++ Python/ByLearn/2 - Listas/3. Fatiando Listas.py
@@ -0,0 +1,9 @@
+#Copiando uma lista para a outra ( Atribuindo Valor)
+
+lista = [1,2,3,4]
+listab = lista
+listab.append(5)
+print(lista)
+print(listab)
+
+
