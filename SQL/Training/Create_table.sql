5fdb4d9684d239fbb06b5a31dcfe964f3fbf22ca
Pedro Maia <pedroasmaia@outlook.com>
2022-04-18 14:57:54 -0300

    int, primary key, varchar

---
+++ SQL/Training/Create_table.sql
@@ -0,0 +1,9 @@
+Create table computer
+
+( 
+    id_computer int primary key, /*primary key é um identificador unico, não podem existir dois itens com o mesmo valor nesta tabela*/
+    marca varchar(15), /*varchar é o tamanho máximo que uma variavel deve ter*/
+    country varchar(2),  /*varchar é o tamanho máximo que uma variavel deve ter*/
+    memory int /*Int define o tipo de dado*/
+
+)
