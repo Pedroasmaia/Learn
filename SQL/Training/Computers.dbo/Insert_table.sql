8c5eef94081e843d680ec3808b855042bb8be403
Pedro Maia <pedroasmaia@outlook.com>
2022-04-18 15:04:04 -0300

    Insert

---
+++ SQL/Training/Computers.dbo/Insert_table.sql
@@ -0,0 +1,2 @@
+insert into computer (id_computer,marca,country,memory) values (1,'Dell','MG',8)
+/*insert into 'tabela'(coluna 1,coluna2,coluna3,coluna4) valores inseridos na(coluna 1,coluna2,coluna3,coluna4)*/
