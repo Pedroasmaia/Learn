335f618813fd7cb5a423a37631e1be0b762fde76
Pedro Maia <pedroasmaia@outlook.com>
2022-06-08 17:49:34 -0300

    GET concluido

--- Python/Training/Footaballdata.org/Artilheiros_SerieA.py
+++ Python/Training/Footaballdata.org/Artilheiros_SerieA.py
@@ -9,16 +9,28 @@ load_dotenv()
 
 api_token = os.environ['key']
 
-year = 2021
+year = input('Qual ano você quer?')
 uri = 'https://api.football-data.org/v4/matches/'
 headers = { 'X-Auth-Token': api_token}
-SerieA_Scores = (f'https://api.football-data.org/v4/competitions/SA/scorers?season{year}')
+SerieA_Scores = (f'https://api.football-data.org/v4/competitions/SA/scorers?season={year}')
 
-# response = r.get(uri, headers=headers)
-# for match in response.json()['matches']:
-# print(match)
-def main():
-    Scores = r.get(SerieA_Scores,headers=headers)
-    Scores_List = Scores.json()
-    print(Scores_List)
-main()
+Scores = r.get(SerieA_Scores,headers=headers)
+retorno = Scores.json() #Json Todo
+
+#Informações da Competição
+competition = retorno['competition']  #Pega 'competition' dentro do Json todo
+competition_name = competition['name'] #Pega 'name' dentro de 'competition'
+print(f'O Nome da liga é {competition_name}')
+
+#Listar os 10 Jogadores
+OrderList = 1
+top_scores = retorno['scorers'] #Pega 'scores' no json todo
+print('Posição | Jogador | Gols')
+for players_block in top_scores: #Separa Bloco de Informações dos Jogadores de 'Scores'
+    players = players_block['player'] #Do bloco de informações individualiza os jogadores
+    player_name = players['name'] #Informa todo os nomes do que foi individualizado
+    player_goals = players_block['goals'] #Pega os gols de cada jogador
+    print(f'{OrderList}° | {player_name} | {player_goals} Gols')
+    OrderList = OrderList + 1
+
+
